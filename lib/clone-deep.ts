e3525207be512437e1b9207388ac8355a2b10493
Alekssandr Brenchev <alexzund3r@yahoo.com>
2019-08-11 20:23:36 +0700

    Implemented cloning for regexps, dates, primitive wrappers, added handling of circular references and retaining of prototype

--- lib/clone-deep.ts
+++ lib/clone-deep.ts
@@ -1,11 +1,27 @@
 import { Dictionary } from "./types";
 
-function cloneDeepArray(arr: any[]): any[] {
+function _clonePrimitiveWrapper(obj: Number | String | Boolean): Number | String | Boolean {
+  const value = obj.valueOf();
+
+  if (obj instanceof Number) {
+    return new Number(value);
+  } else if (obj instanceof String) {
+    return new String(value);
+  }
+
+  return new Boolean(value);
+}
+
+function _cloneRegExps(re: RegExp): RegExp {
+  return new RegExp(re.source, re.flags);
+}
+
+function _cloneArray(arr: any[], alreadyClonedRefs: Map<any, any>): any[] {
   const result: any[] = [];
 
   arr.forEach((elem) => {
     if (typeof elem === 'object') {
-      result.push(cloneDeep(elem));
+      result.push(_clone(elem, alreadyClonedRefs));
     } else {
       result.push(elem);
     }
@@ -14,22 +30,42 @@ function cloneDeepArray(arr: any[]): any[] {
   return result;
 }
 
-export function cloneDeep(obj: Dictionary<any>): Dictionary<any> {
-  const result: Dictionary<any> = {};
+function _clone(obj: any, alreadyClonedRefs: Map<any, any>): any {
+  const result: any = Object.create(Object.getPrototypeOf(obj));
+
+  alreadyClonedRefs.set(obj, result);
+
+  if (obj instanceof Number || obj instanceof Boolean || obj instanceof String) {
+    return _clonePrimitiveWrapper(obj);
+  }
+
+  if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+
+  if (obj instanceof RegExp) {
+    return _cloneRegExps(obj);
+  }
 
   if (Array.isArray(obj)) {
-    return cloneDeepArray(obj);
+    return _cloneArray(obj, alreadyClonedRefs);
   }
 
   Object.keys(obj).forEach((key) => {
-    if (Array.isArray(obj[key])) {
-      result[key] = cloneDeepArray(obj[key]);
+    if (alreadyClonedRefs.has(obj[key])) {
+      result[key] = alreadyClonedRefs.get(obj[key]);
+    } else if (Array.isArray(obj[key])) {
+      result[key] = _cloneArray(obj[key], alreadyClonedRefs);
     } else if (typeof obj[key] === 'object') {
-      result[key] = cloneDeep(obj[key]);
+      result[key] = _clone(obj[key], alreadyClonedRefs);
     } else {
       result[key] = obj[key];
     }
   });
 
   return result;
+}
+
+export function cloneDeep(obj: Dictionary<any>): Dictionary<any> {
+  return _clone(obj, new Map());
 }
--- test/specs/clone-deep.ts
+++ test/specs/clone-deep.ts
@@ -1,5 +1,4 @@
 import { cloneDeep } from '../../lib';
-import clone from 'ramda/es/clone';
 
 describe('cloneDeep', () => {
   it('should clone plain objects with primitive fields', () => {
@@ -52,23 +51,54 @@ describe('cloneDeep', () => {
   });
 
   it('should clone Number objects', () => {
+    const num = new Number(4);
 
+    const clonedNum = cloneDeep(num);
+
+    expect(clonedNum).not.toBe(num);
+    expect(clonedNum.valueOf()).toEqual(num.valueOf());
   });
 
   it('should clone Boolean objects', () => {
+    const bool = new Boolean(true);
+
+    const clonedBool = cloneDeep(bool);
+
+    expect(clonedBool).not.toBe(bool);
+    expect(clonedBool.valueOf()).toEqual(bool.valueOf());
 
   });
 
   it('should clone String objects', () => {
+    const str = new String('test');
 
+    const clonedStr = cloneDeep(str);
+
+    expect(clonedStr).not.toBe(str);
+    expect(clonedStr.valueOf()).toEqual(str.valueOf());
   });
 
   it('should clone Date objects', () => {
+    const now = new Date();
 
+    const clonedNow  = cloneDeep(now);
+
+    expect(clonedNow).not.toBe(now);
+    expect(clonedNow).toEqual(now);
   });
 
   it('should clone RegExp objects', () => {
+    const re = new RegExp(/(\w+)\s(\w+)/, 'ig');
+    
+    const clonedRe = cloneDeep(re);
 
+    expect(clonedRe).not.toBe(re);
+    expect(clonedRe).toEqual(re);
+    expect(clonedRe.ignoreCase).toEqual(re.ignoreCase);
+    expect(clonedRe.sticky).toEqual(re.sticky);
+    expect(clonedRe.global).toEqual(re.global);
+    expect(clonedRe.multiline).toEqual(re.multiline);
+    expect(clonedRe.unicode).toEqual(re.unicode);
   });
 
   it('should clone Map objects', () => {
@@ -96,6 +126,54 @@ describe('cloneDeep', () => {
   });
 
   it('should handle cyclic references', () => {
+    const a: any = {
+      f: 2,
+      c: null
+    };
+    const b = {
+      e: 1,
+      d: a
+    };
+    a.c = b;
+
+    const clonedA = cloneDeep(a);
+
+    expect(clonedA).not.toBe(a);
+    expect(clonedA).toEqual(a);
+
+    b.d = 5;
+    expect(clonedA).not.toEqual(a);
+  });
+
+  it('should retain prototype', () => {
+    class Animal {};
+
+    const dog = new Animal();
+
+    const clonedDog = cloneDeep(dog);
+
+    expect(clonedDog).not.toBe(dog);
+    expect(clonedDog).toEqual(dog);
+    expect(clonedDog instanceof Animal).toEqual(true);
+  });
+
+  it('should clone instances', () => {
+    class Animal {
+      constructor(public age: number) {
+      }
+    };
+
+    const dog = new Animal(5);
+
+    const clonedDog = cloneDeep(dog);
+
+    clonedDog.age = 10;
+
+    expect(dog.age).toEqual(5);
+    expect(clonedDog.age).toEqual(10);
+  });
+
+  it('should clone property descriptors', () => {
 
   });
 });
